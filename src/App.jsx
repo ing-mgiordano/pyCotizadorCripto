aad1c0bccd5e28bbda871e7218df0b214dce3611
ing-mgiordano <ing.mgiordano@gmail.com>
2022-05-29 11:16:19 +0200

    nvo py

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif
+  color: #FFF
+`
+
+function App() {
+  return (
+
+    <Heading>hola</Heading>
+
+  )
+}
+
+export default App;
